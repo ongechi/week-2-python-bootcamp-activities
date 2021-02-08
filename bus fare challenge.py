51a53418a0e5faab245f508367651957670b22c9
ongechi <77966899+ongechi@users.noreply.github.com>
2021-02-08 17:50:48 +0300

    Update bus fare challenge.py

--- bus fare challenge.py
+++ bus fare challenge.py
@@ -1,16 +1,17 @@
+#bus fare challenge program
 import datetime
 import time
 from datetime import date
-
+# date
 today=date.today()
 print("Date:",today)
 print("Day:",datetime.date.today().strftime("%a"))
-
+#date and time
 day=time.strftime("%a")
 week = set(["Mon","Tue","Wed","Thur","Fri"])
 Sat = set(["Sat"])
 Sun = set(["Sun"])
-
+#printing day time and the fare
 if day in week:
     print("Fare: 100")
 elif day in Sat :
